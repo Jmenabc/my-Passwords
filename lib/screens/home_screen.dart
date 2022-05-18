b709fa0f1b9354b5321ecce5da1b7fc2b4baa1f9
javi mena <menajavier2002@gmail.com>
2022-05-18 17:43:41 +0200

    Terminada interfaz gráfica de las contraseñas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,9 +14,9 @@ class _HomesScreenState extends State<HomesScreen> {
     String website = "";
     String usuario = "";
     String password = "";
+    String type = "";
     CollectionReference firestore =
         FirebaseFirestore.instance.collection('users');
-    final data = firestore.get();
 
     // final reference = FirebaseFirestore.instance;
     return Scaffold(
@@ -26,9 +26,13 @@ class _HomesScreenState extends State<HomesScreen> {
                 context: context,
                 builder: (context) {
                   return Card(
-                    margin: const EdgeInsets.all(16),
+                    margin: const EdgeInsets.all(64),
                     child: Column(
                       children: [
+                        const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text('Añadir Cuent@/s'),
+                        ),
                         Padding(
                             padding: const EdgeInsets.all(16),
                             child: TextField(
@@ -57,6 +61,15 @@ class _HomesScreenState extends State<HomesScreen> {
                               decoration: const InputDecoration(
                                   label: Text('Contraseña')),
                             )),
+                        Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: TextField(
+                              onChanged: (type1) {
+                                type = type1;
+                              },
+                              decoration: const InputDecoration(
+                                  label: Text('Contraseña')),
+                            )),
                         const Spacer(),
                         Container(
                             padding: const EdgeInsets.all(8),
@@ -67,7 +80,8 @@ class _HomesScreenState extends State<HomesScreen> {
                                       await firestore.add({
                                         "website": website,
                                         "username": usuario,
-                                        "password": password
+                                        "password": password,
+                                        "type": type
                                       }).then(
                                           (value) => print("Cuenta añadida"));
                                       Navigator.pop(context);
@@ -106,40 +120,85 @@ class _HomesScreenState extends State<HomesScreen> {
             if (!snapshot.hasData) {
               return const Center(child: CircularProgressIndicator());
             }
-            return ListView.builder(
-                itemCount: snapshot.data?.docs.length,
-                itemBuilder: (BuildContext context, int index) {
-                  DocumentSnapshot ds = snapshot.data!.docs[index];
-                  return GestureDetector(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return Dialog(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Text(ds['username']),
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Text(ds['password']),
-                                  )
-                                ],
-                              ),
-                            );
-                          });
-                    },
-                    child: SizedBox(
-                      height: 200,
-                      child: Card(
-                        child: Text(ds['website']),
-                      ),
-                    ),
-                  );
-                });
+            return buildCardContent(snapshot);
           },
         ));
+  }
+
+  ListView buildCardContent(
+      AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+    return ListView.builder(
+        itemCount: snapshot.data?.docs.length,
+        itemBuilder: (BuildContext context, int index) {
+          DocumentSnapshot ds = snapshot.data!.docs[index];
+          // return buildGestureDetector(context, ds);
+          return Card(
+            child: ExpansionTile(
+              title: Text('${ds['website']}'),
+              subtitle: Text('${ds['type']}'),
+              children: <Widget>[
+                const Divider(),
+                ListTile(
+                  leading: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: const Icon(Icons.person_outline_outlined)),
+                  title: Text('Usuario: ${ds['username']}'),
+                ),
+                const Divider(),
+                ListTile(
+                  leading: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: const Icon(Icons.lock_outline_rounded)),
+                  title: Text('Contraseña: ${ds['password']}'),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  GestureDetector buildGestureDetector(
+      BuildContext context, DocumentSnapshot<Object?> ds) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                insetPadding: const EdgeInsets.only(top: 232, bottom: 232),
+                content: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      padding: const EdgeInsets.all(16),
+                      child: Center(child: Text('Usuario: ${ds['username']}')),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Center(
+                          child: Tooltip(
+                        showDuration: const Duration(seconds: 30),
+                        message: 'Contraseña:  ${ds['password']}',
+                        child: const Center(child: Icon(Icons.lock_open)),
+                      )),
+                    )
+                  ],
+                ),
+              );
+            });
+      },
+      child: ShowCard(ds),
+    );
+  }
+
+  SizedBox ShowCard(DocumentSnapshot<Object?> ds) {
+    return SizedBox(
+      height: 72,
+      child: Card(
+        color: Colors.grey,
+        elevation: 8,
+        child: Center(child: Text(ds['website'])),
+      ),
+    );
   }
 }
